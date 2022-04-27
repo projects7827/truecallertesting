70fc103af48a06e5297e32ae586501b3de253675
Prateek <prateek.taluja@whizkidzmedia.com>
2022-04-28 02:18:23 +0530

    g

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ const App = () => {
 
   function truecaller() {
     alert("successful");
-    fetch("https://ockypocky-test.azurewebsites.net/result").then((res) => res.json()).then((value) => {
-      JSON.stringify(value);
-      const options = {
-        method: "GET",
-        url: "https://profile4.truecaller.com/v1/default",
-        headers: { Authorization: `Bearer ${value.accessToken}` },
-      };
-      fetch(options).then((res) => { res.json() }).then((res) => {
-        JSON.stringify(res);
-      })
-
-    })
     window.location.href = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${req_nonce}&partnerKey=IFspJbde6cb8b59a74d28953031a1f34ff29d&partnerName=ockypocky&lang=en&loginPrefix=getdetails&loginSuffix=signin&ctaPrefix=proceedwith&ctaColor=white&ctaTextColor=red&btnShape=round&skipOption=useanothernum`
+
+    // fetch("https://ockypocky-test.azurewebsites.net/result").then((res) => res.json()).then((value) => {
+    //   JSON.stringify(value);
+    //   const options = {
+    //     method: "GET",
+    //     url: "https://profile4.truecaller.com/v1/default",
+    //     headers: { Authorization: `Bearer ${value.accessToken}` },
+    //   };
+    //   fetch(options).then((res) => { res.json() }).then((res) => {
+    //     JSON.stringify(res);
+    //   })
+
+    // })
   }
 
   return (
