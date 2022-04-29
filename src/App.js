0a38af377ab9a8e7ffcdfcdffb67e37291018432
Prateek <prateek.taluja@whizkidzmedia.com>
2022-04-29 12:45:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ const App = () => {
         requestNonce=${req_nonce}
         &partnerKey="QY8jy517b3efdf98d44798e129f973d028a69"
         &partnerName="testing"
-        &lang=en
-        &title=TITLE_STRING_OPTION`;
+        &lang=en`;
 
     setTimeout(function () {
 
       if (document.hasFocus()) {
+        alert("success")
         // Truecaller app not present on the device and you redirect the user 
         // to your alternate verification page
       } else {
+        alert("fail")
+
         // Truecaller app present on the device and the profile overlay opens
         // The user clicks on verify & you'll receive the user's access token to fetch the profile on your 
         // callback URL - post which, you can refresh the session at your frontend and complete the user  verification
