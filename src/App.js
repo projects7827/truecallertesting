2bc02992d8cd08331a8d21937b99993a3d870527
Prateek <prateek.taluja@whizkidzmedia.com>
2022-04-29 12:35:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import React from 'react'
 const App = () => {
   var req_nonce = 123456789598;  // random number of length 8 to 64 characters
   function truecaller() {
-      if (document.hasFocus()) {
-        // window.location.href = "/NotFound";
-        alert("failure ")
-      } else {
+    console.log(document.hasFocus());
+      // if (document.hasFocus()) {
+      //   // window.location.href = "/NotFound";
+      //   alert("failure ")
+      // } else {
  
         alert("success ")
 
-        window.location = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${req_nonce}&partnerKey=QY8jy517b3efdf98d44798e129f973d028a69&partnerName=testing&lang=en&loginPrefix=getdetails&loginSuffix=signin&ctaPrefix=proceedwith&ctaColor=white&ctaTextColor=red&btnShape=round&skipOption=useanothernum`;
+        window.location.href = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${req_nonce}&partnerKey=QY8jy517b3efdf98d44798e129f973d028a69&partnerName=testing&lang=en&loginPrefix=getdetails&loginSuffix=signin&ctaPrefix=proceedwith&ctaColor=white&ctaTextColor=red&btnShape=round&skipOption=useanothernum`;
 
     
-  }}
+  }
   return (
     <>
       <button onClick={truecaller}>truecaller</button>
