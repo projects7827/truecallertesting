2f619acb34b0bbb1084dda7fa5ca1fb723590b42
Prateek <prateek.taluja@whizkidzmedia.com>
2022-04-29 12:38:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import React from 'react'
 const App = () => {
   var req_nonce = 123456789598;  // random number of length 8 to 64 characters
   function truecaller() {
-    console.log(document.hasFocus());
-      // if (document.hasFocus()) {
-      //   // window.location.href = "/NotFound";
-      //   alert("failure ")
-      // } else {
- 
-        alert("success ")
 
-        window.location.href = `truecallersdk://truesdk/web_verify?type=btmsheet&requestNonce=${req_nonce}&partnerKey=QY8jy517b3efdf98d44798e129f973d028a69&partnerName=testing&lang=en&loginPrefix=getdetails&loginSuffix=signin&ctaPrefix=proceedwith&ctaColor=white&ctaTextColor=red&btnShape=round&skipOption=useanothernum`;
+    window.location = `truecallersdk://truesdk/web_verify?
+        requestNonce=${req_nonce}
+        &partnerKey="QY8jy517b3efdf98d44798e129f973d028a69"
+        &partnerName="testing"
+        &lang=en
+        &title=TITLE_STRING_OPTION`;
 
-    
+    setTimeout(function () {
+
+      if (document.hasFocus()) {
+        // Truecaller app not present on the device and you redirect the user 
+        // to your alternate verification page
+      } else {
+        // Truecaller app present on the device and the profile overlay opens
+        // The user clicks on verify & you'll receive the user's access token to fetch the profile on your 
+        // callback URL - post which, you can refresh the session at your frontend and complete the user  verification
+      }
+    }, 600);
+
+
   }
   return (
     <>
