394759d6bae75243ae4f0f8ee6278f8d5c400e27
Prateek <prateek.taluja@whizkidzmedia.com>
2022-05-20 17:43:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ const App = () => {
       if (document.hasFocus()) {
         alert("not found")
       } else {
-        alert("successful");
-        fetch("https://pt-truecaller.herokuapp.com/reply").then((res) => {
-        console.log(res);  
-        return res.json()}).then((value) => {
-          alert(JSON.stringify(value));
-          alert("done")
-          const options = {
-            method: "GET",
-            url: "https://profile4.truecaller.com/v1/default",
-            headers: { Authorization: `Bearer ${value.accessToken}` },
-          };
-          fetch(options).then((res) => { res.json() }).then((res) => {
-            alert(JSON.stringify(res));
-          }).catch((err) => { JSON.stringify(err) })
+        // fetch("https://pt-truecaller.herokuapp.com/reply").then((res) => {
+        // console.log(res);  
+        // return res.json()}).then((value) => {
+        //   alert(JSON.stringify(value));
+        //   alert("done")
+        //   const options = {
+        //     method: "GET",
+        //     url: "https://profile4.truecaller.com/v1/default",
+        //     headers: { Authorization: `Bearer ${value.accessToken}` },
+        //   };
+        //   fetch(options).then((res) => { res.json() }).then((res) => {
+        //     alert(JSON.stringify(res));
+        //   }).catch((err) => { JSON.stringify(err) })
 
-        })
+        // })
       }
     }, 600);
     window.location.href = `truecallersdk://truesdk/web_verify?requestNonce=${req_nonce}&partnerKey="P0smK41384c5915474009aeaadc01110e27af"&partnerName="truecaller"&lang=en`;
