d600d9fcdeb34f288efa9f0926f4c55a0e79bb9d
Prateek <prateek.taluja@whizkidzmedia.com>
2022-05-21 17:36:12 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ const App = () => {
     let max = 123456789598;
     let result = Math.random() * (max - min) + min;
     setTimeout(function () {
-      fetch(`https://ockypockydev.azurewebsites.net/api/v1/user/truecaller_auth/get_token/?req_id=${Math.floor(result)}`).then((res) => res.json()).then((data) => {
-        document.write(JSON.stringify(data));
-      })
-      alert(result)
+
+      if (document.hasFocus()) {
+        // To Check whether Truecaller App is present on device or not
+        // You can add your logic here    
+      }
+      else {
+        // Truecaller app present on the device and the profile overlay opens
+        // The user clicks on verify & you'll receive the user's access token to fetch the profile on your 
+        // callback URL - post which, you can refresh the session at your frontend and complete the user  verification
+        fetch(`https://ockypockydev.azurewebsites.net/api/v1/user/truecaller_auth/get_token/?req_id=${Math.floor(result)}`).then((res) => res.json()).then((data) => {
+          document.write(JSON.stringify(data));
+        })
+        alert(Math.floor(result))
+      }
     }, 1000);
     let deeplink = `truecallersdk://truesdk/web_verify?requestNonce=${Math.floor(result)}&partnerKey=ktAmh7b26e72d999c4f42b5a18e12454dee83&partnerName=ockypocky2`
     window.location.href = deeplink;
