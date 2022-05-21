ba40164efebaafc9387f517387cd10d4d74a688d
Prateek <prateek.taluja@whizkidzmedia.com>
2022-05-21 19:34:59 +0530

    done

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       }
       else {
 
-        document.addEventListener("focus", function (cb) {
+        window.addEventListener("focus", function (cb) {
 
 
           alert("done")
