33eee90b2ec3c3bfeecd23a3962b23c6b14e9187
Prateek <prateek.taluja@whizkidzmedia.com>
2022-05-21 17:20:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,34 +8,14 @@ const App = () => {
     let max = 123456789598;
     let result = Math.random() * (max - min) + min;
     setTimeout(function () {
-      if (document.hasFocus()) {
-      } else {
-        // fetch("https://pt-truecaller.herokuapp.com/reply").then((res) => {
-        // console.log(res);  
-        // return res.json()}).then((value) => {
-        //   alert(JSON.stringify(value));
-        //   alert("done")
-        //   const options = {
-        //     method: "GET",
-        //     url: "https://profile4.truecaller.com/v1/default",
-        //     headers: { Authorization: `Bearer ${value.accessToken}` },
-        //   };
-        //   fetch(options).then((res) => { res.json() }).then((res) => {
-        //     alert(JSON.stringify(res));
-        //   }).catch((err) => { JSON.stringify(err) })
-
-        // })
-      }
-    }, 2000);
-    let deeplink = `truecallersdk://truesdk/web_verify?requestNonce=12345678000&partnerKey=ktAmh7b26e72d999c4f42b5a18e12454dee83&partnerName=ockypocky2`
-    // alert(deeplink);
-
+      fetch(`https://ockypockydev.azurewebsites.net/api/v1/user/truecaller_auth/get_token/?req_id=${Math.floor(result)}`).then((res) => res.json()).then((data) => {
+        console.log(data)
+      })
+      alert(result)
+    }, 1000);
+    let deeplink = `truecallersdk://truesdk/web_verify?requestNonce=${Math.floor(result)}&partnerKey=ktAmh7b26e72d999c4f42b5a18e12454dee83&partnerName=ockypocky2`
     window.location.href = deeplink;
-
-
-
   }
-
   return (
     <>
       <button onClick={truecaller}>truecaller</button>
