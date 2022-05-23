33f3d68af300629df8435faa0ff83a84ee74fd12
Prateek <prateek.taluja@whizkidzmedia.com>
2022-05-23 16:47:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -10,21 +10,41 @@ const App = () => {
 
 
   function truecaller() {
-    let min = 100000000000;
-    let max = 123456789598;
-    let result = Math.random() * (max - min) + min;
-    let deeplink = `truecallersdk://truesdk/web_verify?requestNonce=${Math.floor(result)}&partnerKey=ktAmh7b26e72d999c4f42b5a18e12454dee83&partnerName=ockypocky2`
-    window.location.href = deeplink;
-    setTimeout(() => {
-      if (document.hasFocus()) {
-        alert("has focus")
-        // To Check whether Truecaller App is present on device or not
-        // You can add your logic here    
+
+    function getMobileOperatingSystem() {
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+      // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+          alert("Windows Phone");
       }
-      else {
-        settingFocus(result);
+  
+      if (/android/i.test(userAgent)) {
+        alert("Android");
       }
-    }, 500);
+  
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        alert("iOS");
+      }
+  
+      return "unknown";
+  }
+    // let min = 100000000000;
+    // let max = 123456789598;
+    // let result = Math.random() * (max - min) + min;
+    // let deeplink = `truecallersdk://truesdk/web_verify?requestNonce=${Math.floor(result)}&partnerKey=ktAmh7b26e72d999c4f42b5a18e12454dee83&partnerName=ockypocky2`
+    // window.location.href = deeplink;
+    // setTimeout(() => {
+    //   if (document.hasFocus()) {
+    //     alert("has focus")
+    //     // To Check whether Truecaller App is present on device or not
+    //     // You can add your logic here    
+    //   }
+    //   else {
+    //     settingFocus(result);
+    //   }
+    // }, 500);
   }
 
   function settingFocus(result) {
@@ -34,27 +54,7 @@ const App = () => {
         fetch(`https://ockypockydev.azurewebsites.net/api/v1/user/truecaller_auth/get_token/?req_id=${Math.floor(result)}`).
           then((res) => res.json()).
           then((data) => {
-            console.log(data.end_point)
-            console.log(data.access_token)
-
-            let myHeaders = new Headers(
-              { 'Content-Type': 'text/xml' },
-              { 'Accept': "application/json" },
-              { "Content-Type": "application/json" },
-              { "Access-Control-Allow-Methods": "GET, POST, OPTIONS" },
-              { "Access-Control-Allow-Origin": "*" },
-              { "Access-Control-Allow-Credentials": "true" },
-              { "Authorization": `Bearer ${data.access_token}` }
-            );
-            const opt = {
-              mode: "cors",
-              headers: myHeaders
-            }
-            fetch("https://profile4-noneu.truecaller.com/v1/default", opt).then((res) => res.json()).then((data) => {
-              console.log(data);
-            }).catch((err) => {
-              console.log(err);
-            })
+            alert(JSON.stringify(data));  
           })
       }
       else {
